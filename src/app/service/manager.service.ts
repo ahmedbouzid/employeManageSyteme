a6ca44ffea9795f263fd445c8c61b619dcf14dbe
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-21 17:15:17 +0200

    create meploye Service

--- src/app/service/manager.service.ts
+++ src/app/service/manager.service.ts
@@ -1,14 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EmployeDto } from '../models/EmployeDto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ManagerService {
-  URL = 'http://localhost:3000/posts'
+  URL = 'http://localhost:3000/posts/'
   constructor(private http : HttpClient) { }
 
   getAllEmployee() {
-    return this.http.get(this.URL);
+    return this.http.get<EmployeDto[]>(this.URL);
+  }
+  postEmploye(payload : EmployeDto) {
+    return this.http.post<EmployeDto>(this.URL , payload)
+  }
+  deleteEmployee(id : string) {
+    return this.http.delete(this.URL + '/'+ id)
+  }
+  editEmploye() {
+    
   }
 }
