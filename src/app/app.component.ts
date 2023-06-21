73b611047dc9398af41273c9393b79e8bb12267f
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-21 13:02:05 +0200

    prepare dialog For employe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss' ,
+
+]
 })
 export class AppComponent {
   title = 'EmployeManage';
+  display = "none" ;
+  openModal() {
+    this.display = "block"
+  }
+  onCloseHandled(){
+    this.display = "none"
+  }
+  constructor(){
+
+  }
 }
