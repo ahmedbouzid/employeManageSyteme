fbb4ed97ec6da577fb52cac01d285a28e9bd194d
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-21 17:10:24 +0200

    create DTO

---
+++ src/app/models/EmployeDto.model.ts
@@ -0,0 +1,12 @@
+export class EmployeDto {
+  id : number =0 ;
+  firstName : string ='' ;
+  lastName : string = '' ;
+  birthday : string = '' ;
+  gender:string='';
+  educattion : string ='';
+  profileImage:string = '' ;
+  company:string ='';
+  jobxperience :number =0 ;
+  salary:number = 0 ;
+}
--- src/app/service/manager.service.ts
+++ src/app/service/manager.service.ts
@@ -7,4 +7,8 @@ import { Injectable } from '@angular/core';
 export class ManagerService {
   URL = 'http://localhost:3000/posts'
   constructor(private http : HttpClient) { }
+
+  getAllEmployee() {
+    return this.http.get(this.URL);
+  }
 }
